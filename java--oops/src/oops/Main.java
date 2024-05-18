7ee3496af4a3c0d0a92c6d553b0e6f00f3e20603
D RaHuL RoY <drahulroy@RaHuLs-MacBook-Pro.local>
2024-05-18 23:25:50 +0530

    constructer

---
+++ java--oops/src/oops/Main.java
@@ -0,0 +1,47 @@
+package oops;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Declaring a variable of type Student
+        Student s1;
+        // Creating an instance of Student using the default constructor and assigning it to s1
+        s1 = new Student();
+
+        // Setting properties for the student object s1
+        s1.studentId = 1;
+        s1.studentname = "ram";
+        s1.studentage = 54;
+
+        // Calling methods on the student object s1
+        s1.study();
+        s1.showFullDetails();
+
+        // Declaring another variable of type Student
+        Student s2;
+        // Creating an instance of Student using the parameterized constructor with one argument and assigning it to s2
+        s2 = new Student(2);
+
+        // Setting properties for the student object s2
+        s2.studentId = 2;
+        s2.studentname = "sahm";
+        s2.studentage = 34;
+
+        // Calling methods on the student object s2
+        s2.study();
+        s2.showFullDetails();
+
+        // Declaring another variable of type Student
+        Student s3;
+        // Creating an instance of Student using the parameterized constructor with two arguments and assigning it to s3
+        s3 = new Student(10, "raju");
+
+        // Calling methods on the student object s3
+        s3.study();
+        s3.showFullDetails();
+
+        // Demonstrating method overloading
+        s3.study(2); // Calls the overloaded study method with one argument
+        s3.study("math"); // Calls the overloaded study method with one string argument
+    }
+}
--- java--oops/src/oops/Student.java
+++ java--oops/src/oops/Student.java
@@ -11,19 +11,50 @@ class Student {
     String studentcourse;
     String studentphone;
 
+    // Default constructor
+    public Student() {
+        // Constructor body
+        System.out.println("I am first");
+    }
+
+    // Parameterized constructor with one argument
+    public Student(int s) {
+        // Constructor body
+        this.studentId = s;
+        System.out.println("I am 2nd");
+    }
+
+    // Parameterized constructor with two arguments
+    public Student(int id, String name) {
+        // Initializing instance variables with the provided arguments
+        this.studentId = id;
+        this.studentname = name;
+        System.out.println("I am 3rd");
+    }
+
     // Method to simulate studying
     public void study() {
-        System.out.println("Student id " + studentId + " " + studentname + " is studying in this university");
+        System.out.println("Student id " + this.studentId + " " + this.studentname + " is studying in this university");
+    }
+
+    // Overloaded study method with one int argument
+    public void study(int hours) {
+        System.out.println("Student id " + this.studentId + " " + this.studentname + " is studying for " + hours + " hours");
+    }
+
+    // Overloaded study method with one String argument
+    public void study(String subject) {
+        System.out.println("Student id " + this.studentId + " " + this.studentname + " is studying " + subject);
     }
 
     // Method to display full details of the student
     public void showFullDetails() {
-        System.out.println("Student Id: " + studentId);
-        System.out.println("Student Name: " + studentname);
-        System.out.println("Student age: " + studentage);
-        System.out.println("Student Gender: " + studentgender);
-        System.out.println("Student Major: " + studentmajor);
-        System.out.println("Student Course: " + studentcourse);
-        System.out.println("Student Phone: " + studentphone);
+        System.out.println("Student Id: " + this.studentId);
+        System.out.println("Student Name: " + this.studentname);
+        System.out.println("Student age: " + this.studentage);
+        System.out.println("Student Gender: " + this.studentgender);
+        System.out.println("Student Major: " + this.studentmajor);
+        System.out.println("Student Course: " + this.studentcourse);
+        System.out.println("Student Phone: " + this.studentphone);
     }
 }
