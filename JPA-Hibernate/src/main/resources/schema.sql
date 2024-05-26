f147ec35578a8e16d55ae21d2975381c893d4e98
D RaHuL RoY <drahulroy@RaHuLs-MacBook-Pro.local>
2024-05-27 02:09:48 +0530

    spring JPA + JDBC

--- JPA-Hibernate/src/main/resources/schema.sql
+++ JPA-Hibernate/src/main/resources/schema.sql
@@ -1,9 +1,8 @@
-create table course
+create table course 
 (
-    CourseID INT PRIMARY KEY AUTO_INCREMENT,
-    CourseName VARCHAR(255) NOT NULL,
-    AutherName VARCHAR(255) NOT NULL,
-    primary key (CourseID)
+	id bigint not null,
+	name varchar(255) not null,
+	author varchar(255) not null,
+	primary key (id)
 
 );
-
